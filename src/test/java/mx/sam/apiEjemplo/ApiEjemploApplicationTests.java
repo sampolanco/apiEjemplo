9395b407bb3445342ac160785e62e460a309ede8
sampolanco <samy_ckl@hotmail.com>
2020-05-24 02:25:32 -0500

    primer commit

---
+++ src/test/java/mx/sam/apiEjemplo/ApiEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.sam.apiEjemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
