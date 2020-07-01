303f82457e3b1942bab8e1e962f8ba0b027ea8a1
sampolanco <samy_ckl@hotmail.com>
2020-07-01 03:01:27 -0500

    se corrige gitIgnore

--- src/main/java/mx/sam/apiEjemplo/controller/api/usuario/UsuarioController.java
+++ src/main/java/mx/sam/apiEjemplo/controller/api/usuario/UsuarioController.java
@@ -3,6 +3,7 @@ package mx.sam.apiEjemplo.controller.api.usuario;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,8 @@ import mx.sam.apiEjemplo.dto.UsuarioDTO;
 import mx.sam.apiEjemplo.service.IUsuarioService;
 
 @RestController
-@RequestMapping(path="/Usuario")
+@CrossOrigin(origins= {"http://localhost:4200"})
+@RequestMapping(path="/usuario")
 public class UsuarioController {
 	@Autowired
 	IUsuarioService service;
